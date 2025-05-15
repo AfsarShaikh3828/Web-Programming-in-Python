e11b2ca500cb1afeba37d18edf68afa4ae1d9b4c
Afsar Shaikh <shaikhafsar3828@gmail.com>
2025-05-15 21:08:51 +0530

    Add files via upload

---
+++ Assignment1 string.py
@@ -0,0 +1,7 @@
+#String Declaraction
+s = "afsar\nAkram\nshaikh"
+
+#s.replace() method to remove all newline characters from a string. It replaces every occurrence \n with an empty string.
+text = s.replace("\n", " ")
+#print the clean text
+print(text)
